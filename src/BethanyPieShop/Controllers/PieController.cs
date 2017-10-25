81f65a2a705a1bbe3e817b9b99ef33dfefef07e4
Joel Ramos <joelalexanderramos@gmail.com>
2017-10-25 16:01:26 -0400

    Initial Load

---
+++ src/BethanyPieShop/Controllers/PieController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BethanyPieShop.Models;
+using BethanyPieShop.ViewModels;
+
+namespace BethanyPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepositry)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepositry;
+        }
+
+        public ViewResult List()
+        {
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+
+            piesListViewModel.Pies = _pieRepository.Pies;
+            piesListViewModel.CurrentCategory = "Cheese cakes";
+            return View(piesListViewModel);
+        }
+
+        public ViewResult Index()
+        {
+            ViewBag.Message = "Welcome to Bethany's Pie Shop";
+            return View();
+        }
+    }
+}
